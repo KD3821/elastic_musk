335fe7df65d5881728a4535e54a9c19880e05153
KD3821 <80853241+KD3821@users.noreply.github.com>
2022-04-07 12:52:54 +0300

    some template update done

--- forms.py
+++ forms.py
@@ -6,5 +6,5 @@ from wtforms.validators import DataRequired
 
 class SearchForm(FlaskForm):
     name_s = StringField("Choose index", validators=[DataRequired()])
-    body_s = TextAreaField("ES format request without 'body='", validators=[DataRequired()])
+    body_s = TextAreaField("Enter your request {...}", validators=[DataRequired()])
     submit = SubmitField("run search")
